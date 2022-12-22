74bd1936a1a7b76044a772e7bb5a2ca31995fe6f
lisa-butler <l.butler1993@gmail.com>
2022-12-22 14:14:33 +0000

    Add product admin page

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('courses/', views.courses, name='courses'),
     path('qualified/', views.qualified, name='qualified'),
     path('gear/', views.gear, name='gear'),
+    path('add/', views.add_product, name='add_product'),
 ]
